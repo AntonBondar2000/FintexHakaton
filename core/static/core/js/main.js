8a297c7a654e1f1a00cfc8661b5468f9c4548692
AntonBondar2000 <abondar@tayle.ru>
2021-09-04 10:24:02 +0300

    add chart

--- core/static/core/js/main.js
+++ core/static/core/js/main.js
@@ -1,21 +1,16 @@
 var all_collor = []
-function getRandomColor() {
-    var letters = '0123456789ABCDE'.split('');
-    var color = '#';
-    for (var i = 0; i < 6; i++ ) {
-        color += letters[Math.floor(Math.random() * 16)];
-    }
-    if (all_collor.find(el=>el==color) != undefined){
-        return getRandomColor()
-    }
-    all_collor.push(color)
-    return color;
+function distinctColors(count) {
+    var r = Math.floor(Math.random()*255);
+    var g = Math.floor(Math.random()*255);
+    var b = Math.floor(Math.random()*255);
+    return "#"+r.toString(16) + g.toString(16) + b.toString(16)
 }
+
 let keys_branch = Object.keys(branchData)
 let values_branch = Object.values(branchData)
 let colors_branch = []
 for (let i = 0; i<keys_branch.length; i++){
-    colors_branch.push(getRandomColor())
+    colors_branch.push(distinctColors(16))
 }
 var ctx = document.getElementById('branch').getContext('2d');
 var chart = new Chart(ctx, {
@@ -34,7 +29,7 @@ let keys_type = Object.keys(typeData)
 let values_type = Object.values(typeData)
 let colors_type = []
 for (let i = 0; i<keys_type.length; i++){
-    colors_type.push(getRandomColor())
+    colors_type.push(distinctColors(16))
 }
 var ctx = document.getElementById('type').getContext('2d');
 var chart = new Chart(ctx, {
@@ -54,7 +49,7 @@ let keys_currency = Object.keys(currencyData)
 let values_currency = Object.values(currencyData)
 let colors_currency = []
 for (let i = 0; i<keys_currency.length; i++){
-    colors_currency.push(getRandomColor())
+    colors_currency.push(distinctColors(16))
 }
 var ctx = document.getElementById('currency').getContext('2d');
 var chart = new Chart(ctx, {
